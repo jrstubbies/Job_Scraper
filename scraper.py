a9e5f19d36afee30ec35b11ff7f7c26fb625f988
Myles Stubbs <jrstubbies@gmail.com>
2024-11-29 18:34:59 +1300

    modified:   scraper.py

--- scraper.py
+++ scraper.py
@@ -3,10 +3,10 @@ from bs4 import BeautifulSoup
 from selenium import webdriver      # as page is dynamic, use selenium to scrape the rendered page
 from selenium.webdriver.chrome.options import Options
 import time
+import random
 
-
+# some headers to help avoid authentication issues
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
-
 
 # prevents error about not finding usb drive ?????   -> "--headless=new" stops program working
 driver_options = Options()
@@ -20,17 +20,16 @@ URL = "https://nz.indeed.com/jobs?q=software+developer&l=New+Zealand"
 driver = webdriver.Chrome(options = driver_options)
 driver.get(URL)
 
-time.sleep(10)   #sleep to allow the results to fully load
-
+#sleep to allow the results to fully load
+time.sleep(3)   
 
 # use '.content' instead of '.text' as it holds raw bytes. Close the web driver
 soup = BeautifulSoup(driver.page_source, "html.parser")
 
-
 # results holds the container for all the 'job cards'
 results = soup.find(id="mosaic-provider-jobcards")
 
-# find all jobs cards within this container, as giving by a div class of 'job_seen_beacon'
+# find all jobs cards within this container
 job_cards = results.find_all('div', class_='job_seen_beacon')
 
 
@@ -38,23 +37,27 @@ job_cards = results.find_all('div', class_='job_seen_beacon')
 # starting page (page 1) does NOT have a 'start' URL parameter
 for job in job_cards:
 
-    # h2 class will have the job name, which is also a link to the full job listing
+    # h2 class will have the job name
     job_title = job.find("h2", class_=lambda text: "jobTitle" in text)
 
+    # get the company name
     company_container = job.find('a', {'class': 'css-1ioi40n e19afand0'})
     company = company_container.text.strip() if company_container else None
 
+    # get the jobs location 
     location_container = job.find('div', {'data-testid': 'inlineHeader-companyLocation'})
     location = location_container.div.text.strip() if location_container else 'Remote or Unknown'
 
+    # get the link for the apply button. This requires a login 
     apply_container = job.find('div', class_ = 'css-199trha eu4oa1w0')
     apply_link = apply_container.find("button")
     apply_url = apply_link["href"]
 
+    # get the job's description
     description_container = job.find(id="jobDescriptionText")
     description = description_container.text.strip()
 
-
+    # print all this information to the console. Change this to print to a file????
     print(job_title.text)
     #print(f"Job URL: {job_title_url}\n")
     print(company)
@@ -62,5 +65,8 @@ for job in job_cards:
     print(apply_url)
     print(description)
     print()
+
+    # helps prevent sending multiple requests in quick succession, helps avoid authentication issues ??
+    time.sleep(random.uniform(3, 7))
 
 driver.quit()
